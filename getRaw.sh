1cc4be82d5cd69de6b90b0ce0fad2bef42e840ce
Martin <martin.survilo@mif.stud.vu.lt>
2023-05-25 22:37:15 +0300

    New file that downloads raw data

---
+++ getRaw.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+wget -P /home/bioinformatikai/HW2/raw ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR204/ERR204044/ERR204044_1.fastq.gz
+wget -P /home/bioinformatikai/HW2/raw ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR204/ERR204044/ERR204044_2.fastq.gz
+wget -P /home/bioinformatikai/HW2/raw ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR151/030/SRR15131330/SRR15131330_1.fastq.gz
+wget -P /home/bioinformatikai/HW2/raw ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR151/030/SRR15131330/SRR15131330_2.fastq.gz
+wget -P /home/bioinformatikai/HW2/raw ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR182/064/SRR18214264/SRR18214264_1.fastq.gz
+wget -P /home/bioinformatikai/HW2/raw ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR182/064/SRR18214264/SRR18214264_2.fastq.gz
